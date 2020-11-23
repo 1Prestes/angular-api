90c89360fdb172d0ef7de058d98075513c6d4679
Dijango Alves <dijango_ilha@hotmail.com>
2020-11-23 15:01:27 -0300

    docs: initial setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'c7002f2710aee7c2ee5e6face0f9e7a2',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'c7002f2710aee7c2ee5e6face0f9e7a2',
 };
 
 /*
